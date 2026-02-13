fed8d03ec17d6b0db2e82c8e8125855e660f9e5f
dangdungvn <tungmj1605@gmail.com>
2026-02-13 23:07:10 +0700

    feat: Replace bottom navigation bar icons with SVG assets for better scalability

--- lib/modules/home/views/home_view.dart
+++ lib/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_platform_ui/adaptive_platform_ui.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../../widgets/upload_progress_overlay.dart';
@@ -36,46 +37,62 @@ class HomeView extends GetView<HomeController> {
                 AdaptiveNavigationDestination(
                   icon: PlatformInfo.isIOS26OrHigher()
                       ? "house"
-                      : 'assets/icons/home_bottom_navbar.svg',
+                      : SvgPicture.asset('assets/icons/home_bottom_navbar.svg'),
                   selectedIcon: PlatformInfo.isIOS26OrHigher()
                       ? "house.fill"
-                      : 'assets/icons/home_bottom_navbar.svg',
+                      : SvgPicture.asset('assets/icons/home_bottom_navbar.svg'),
                   label: 'home_bottom_navbar'.tr,
                 ),
                 AdaptiveNavigationDestination(
                   icon: PlatformInfo.isIOS26OrHigher()
                       ? "location"
-                      : 'assets/icons/location_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/location_bottom_navbar.svg',
+                        ),
                   selectedIcon: PlatformInfo.isIOS26OrHigher()
                       ? "location.fill"
-                      : 'assets/icons/location_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/location_bottom_navbar.svg',
+                        ),
                   label: 'map_bottom_navbar'.tr,
                 ),
                 AdaptiveNavigationDestination(
                   icon: PlatformInfo.isIOS26OrHigher()
                       ? "camera"
-                      : 'assets/icons/camera_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/camera_bottom_navbar.svg',
+                        ),
                   selectedIcon: PlatformInfo.isIOS26OrHigher()
                       ? "camera.fill"
-                      : 'assets/icons/camera_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/camera_bottom_navbar.svg',
+                        ),
                   label: '',
                 ),
                 AdaptiveNavigationDestination(
                   icon: PlatformInfo.isIOS26OrHigher()
                       ? "magnifyingglass"
-                      : 'assets/icons/discovery_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/discovery_bottom_navbar.svg',
+                        ),
                   selectedIcon: PlatformInfo.isIOS26OrHigher()
                       ? "magnifyingglass"
-                      : 'assets/icons/discovery_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/discovery_bottom_navbar.svg',
+                        ),
                   label: 'discovery_bottom_navbar'.tr,
                 ),
                 AdaptiveNavigationDestination(
                   icon: PlatformInfo.isIOS26OrHigher()
                       ? "bubble.left.and.bubble.right"
-                      : 'assets/icons/chatbot_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/chatbot_bottom_navbar.svg',
+                        ),
                   selectedIcon: PlatformInfo.isIOS26OrHigher()
                       ? "bubble.left.and.bubble.right.fill"
-                      : 'assets/icons/chatbot_bottom_navbar.svg',
+                      : SvgPicture.asset(
+                          'assets/icons/chatbot_bottom_navbar.svg',
+                        ),
                   label: 'chatbot_bottom_navbar'.tr,
                 ),
               ],
